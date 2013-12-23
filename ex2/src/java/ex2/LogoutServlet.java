f3f7c79d7027f407a64d5eb6ccf243533c29843f
Gil Mizrahi <suppiral@yahoo.com>
2013-12-23 22:31:23 +0200

    LoginServlet + LogoutServlet works

--- ex2/src/java/ex2/LogoutServlet.java
+++ ex2/src/java/ex2/LogoutServlet.java
@@ -4,9 +4,7 @@
  */
 package ex2;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,8 +14,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author gilmi
  */
-@WebServlet(name = "MainServlet", urlPatterns = {"/MainServlet"})
-public class MainServlet extends MyServlet {
+@WebServlet(name = "LogoutServlet", urlPatterns = {"/LogoutServlet"})
+public class LogoutServlet extends MyServlet {
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -32,26 +30,8 @@ public class MainServlet extends MyServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-                Boolean isLoggedIn = (Boolean)request.getSession().getAttribute("loggedIn");
-        {
-	    response.setContentType("text/html;charset=UTF-8");
-	    PrintWriter out = response.getWriter();
-	    try {
-		if (isLoggedIn == null || isLoggedIn == false)
-		    response.sendRedirect("MainServlet");
-		else {
-		    out.println(getFileContent(HEADER_HTML_FILEPATH));
-		    out.println(getFileContent(HEADER_LOGOUT_FILEPATH));
-		    out.println("nothing");
-		    /* todo: add include to other servlets */
-		    out.println(getFileContent(FOOTER_HTML_FILEPATH));
-		}
-	    } catch (FileNotFoundException e) { 
-		System.err.println(e.toString()); 
-		response.sendError(HttpServletResponse.SC_NO_CONTENT);
-	    }
-	    finally { try  { out.close(); } catch (Exception e) {} }
-	}
+  
+	logout(request, response);
     }
 
     /**
@@ -66,7 +46,14 @@ public class MainServlet extends MyServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+	
+	logout(request, response);
+    }
+    private void logout(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+	
+	request.getSession().setAttribute("loggedIn", false);
+	response.sendRedirect("LoginServlet");
     }
 
     /**
